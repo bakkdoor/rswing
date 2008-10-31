100cdb7d79affd3f12ac6b7345efd015457b6a89
Christopher Bertels <bakkdoor@flasht.de>
2008-10-31 13:35:51 +0100

    changed example-programm in main.rb to something more useful

--- lib/main.rb
+++ lib/main.rb
@@ -1,64 +1,56 @@
 module Main
+  
+  require "rubygems"
   require "rswing"
 
   include RSwing::Components
-  BorderLayout = java.awt.BorderLayout
-  # geht genauso wie die 'normale' variante:
-  # frame = Frame.new("hallo, welt")
-  # ... # hier dann einfach auch zugriffe auf frame
-  Frame.new("hallo, welt", :size => [800, 600], :location => :center) do |frame|
+
+  GridBagLayout = java.awt.GridBagLayout
+
+  frame = Frame.new "Numberconverter",
+                    :size => [400, 100], 
+                    :location => :center do |frame|
+
     frame.default_close_operation = :exit_on_close
 
-    Panel.new(:name => :panel, :belongs_to => frame, :layout_manager => BorderLayout.new) do |panel|
-      Button.new("test", :belongs_to => panel, :name => :testButton, :layout => BorderLayout::NORTH) do |btn|
-        btn.on_click do
-          Dialog.showOption "bitte auswählen:", :option_type => :yes_no, :option_values => ["Aha", "ohno"],
-            :title => "auswahl treffen!", :belongs_to => frame
-        end
+    Panel.new :name => :panel,
+              :belongs_to => frame, 
+              :layout_manager => GridBagLayout.new do |panel|
 
-        btn.on_click do
-          Dialog.show "mein text", 
-                      :dialog_type => :error, 
-                      :title => "hello, world", 
-                      :belongs_to => frame,
-                      :location => :center
-        end
+      TextField.new :text => "0", 
+                    :columns => 10, 
+                    :belongs_to => panel, 
+                    :name => :entry_field
 
+      Button.new  "Convert", 
+                  :belongs_to => panel, 
+                  :name => :button
 
-        btn.on_focus do
-          puts "button hat jetzt focus"
-        end
-
-        btn.on_focus_lost do
-          puts "button hat focus verloren"
-        end
-      end
-
-      puts "text von testButton: #{panel[:testButton].text}"
+      TextField.new :text => "Output", 
+                    :columns => 10, 
+                    :belongs_to => panel, 
+                    :name => :output_field
     end
-
-    #frame.size = [400, 400]
-
-    options = ["Herr", "Frau", "Geek"]
-    selected_value = Dialog.showOption(options.join(" oder ") + "?",
-      :option_values => options, :option_type => :yes_no_cancel, :belongs_to => frame)
-    
-    puts "#{selected_value} wurde ausgewählt"
 
     frame.visible = true
   end
 
-  # dialog testen
-  Dialog.new("mein titel", :modal => true, :location => :center, :size => [500,500]) do |dial|
-    Button.new("click me", :belongs_to => dial, :name => :clickButton) do |button|
-      button.on_click do
-        Dialog.show "test dialog", 
-                    :dialog_type => :info, 
-                    :title => "hey, was geht?"
-      end
-    end
-    
-    dial.visible = true
+  # eventhandler code
+  panel = frame[:panel]
+  panel[:button].on_click do
+     result = Dialog.showOption "Please select:", 
+                                :option_type => :yes_no, 
+                                :option_values => ["HEX", "BINARY"],
+                                :title => "Pick an option!", 
+                                :belongs_to => frame
+
+     base = 10 # standardwert
+     if result == "HEX"
+       base = 16
+     else
+       base = 2
+     end
+     panel[:output_field].text = panel[:entry_field].text.to_i.to_s(base)
   end
-  
+
 end
